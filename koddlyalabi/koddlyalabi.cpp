98bb8e594c917d52006b0c8128b4ec5a2905624b
79674 <79674@DESKTOP-G1VLC7Q>
2022-01-19 14:52:25 +0300

    Добавьте файлы проекта.

---
+++ koddlyalabi/koddlyalabi.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, i;
+	cout << "Введите n" << endl;
+	cin >> n;
+	int* a = new int[n];
+	cout << "Введите элементы массива" << endl;
+	for (i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		a[i] = a[i] * a[i];
+	}
+	for (i = 1; i < n; i++)
+	{
+		int temp = a[i];
+		int j = i - 1;
+		for (; j > -1 && a[j] > temp; j--)
+		{
+			a[j + 1] = a[j];
+		}
+		a[j + 1] = temp;
+	}
+	for (i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+}
